693ed33a50f34c49f0f4b388d700f58a7d766254
Dominik Malinovsky <malca9616@gmail.com>
2021-08-29 13:34:37 +0200

    Escape From Naraka - Stage Loader V1.1
    - Added status if stage was loaded

--- EFN_StageLoader/StageLoader.xaml.cs
+++ EFN_StageLoader/StageLoader.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+//using Memory;
 
 namespace EFN_StageLoader
 {
@@ -24,22 +25,74 @@ namespace EFN_StageLoader
         string fileName = string.Empty;
         string destFile = string.Empty;
 
-        int xd = 0;
-
+        //Mem m = new Mem();
+        //bool openProc = false;
+        //int PID = 0;
+        //float xPos, yPos, zPos;
+        //bool PlayerControl = false;
+        //int pc = 2;
         public StageLoader()
         {
             InitializeComponent();
 
-            DispatcherTimer Timer = new DispatcherTimer();
-            Timer.Tick += Timer_Tick;
-            Timer.Interval = new TimeSpan(0, 0, 1);
-            Timer.Start();
+            //DispatcherTimer Timer = new DispatcherTimer();
+            //Timer.Tick += Timer_Tick;
+            //Timer.Interval = new TimeSpan(0, 0, 1);
+            //Timer.Start();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private void Timer_Tick(object sender, EventArgs e)
+        //{
+        //    PID = m.GetProcIdFromName("Leyak-Win64-Shipping");
+        //    Console.WriteLine(PID);
+
+        //    if (PID > 0) openProc = m.OpenProcess(PID);
+
+        //    if (openProc && PID != 0)
+        //    {
+
+        //        if (pc == 0)
+        //        {
+        //            PlayerControl = true;
+        //            lbl_cm.Foreground = Brushes.Green;
+        //            lbl_cm.Content = PlayerControl;
+
+        //        }
+        //        else
+        //        {
+        //            PlayerControl = false;
+        //            lbl_cm.Foreground = Brushes.Red;
+        //            lbl_cm.Content = PlayerControl;
+        //        }
+        //        pc = m.ReadByte("Leyak-Win64-Shipping.exe+04733CC8,0x331");
+        //        xPos = m.ReadFloat("Leyak-Win64-Shipping.exe+0472A578,0x0,0x20,0x290,0x1d0");
+        //        yPos = m.ReadFloat("Leyak-Win64-Shipping.exe+0472A578,0x0,0x20,0x290,0x1d4");
+        //        zPos = m.ReadFloat("Leyak-Win64-Shipping.exe+0472A578,0x0,0x20,0x290,0x1d8");
+        //        lbl_pos.Content = "X = " + Math.Round(xPos/100,2) + " | Y = " + Math.Round(yPos/100,2) + " | Z = " + Math.Round(yPos / 100, 2);
+                
+                
+        //    }
+        //    else
+        //    {
+        //        PlayerControl = false;
+        //        lbl_cm.Content = "N/A";
+        //        lbl_cm.Foreground = Brushes.Red;
+        //        lbl_pos.Content = "X = ? | Y = ? | Z = ?";
+        //    }
+        //}
 
         private void btn_st1_Click(object sender, RoutedEventArgs e)
         {
@@ -53,6 +106,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st1.Content + " Loaded";
             }
             else
             {
@@ -72,6 +126,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st2.Content + " Loaded";
             }
             else
             {
@@ -90,6 +145,7 @@ namespace EFN_StageLoader
                     fileName = System.IO.Path.GetFileName(s);
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
+                    lbl_st.Content = btn_st3.Content + " Loaded";
                 }
             }
             else
@@ -110,6 +166,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st4.Content + " Loaded";
             }
             else
             {
@@ -129,6 +186,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st5.Content + " Loaded";
             }
             else
             {
@@ -147,6 +205,7 @@ namespace EFN_StageLoader
                     fileName = System.IO.Path.GetFileName(s);
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
+                    lbl_st.Content = btn_st6.Content + " Loaded";
                 }
             }
             else
@@ -167,6 +226,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st7.Content + " Loaded";
             }
             else
             {
@@ -186,6 +246,7 @@ namespace EFN_StageLoader
                     destFile = System.IO.Path.Combine(SavedGames, fileName);
                     File.Copy(s, destFile, true);
                 }
+                lbl_st.Content = btn_st8.Content + " Loaded";
             }
             else
             {
